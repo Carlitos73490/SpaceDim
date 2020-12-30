d7c0a16986cb52714ae87b77c690ee0babf9f26c
Carlitos73490 <71330622+Carlitos73490@users.noreply.github.com>
2020-12-30 13:02:18 +0100

    Binding Login -> Waiting Room

--- app/src/main/java/com/example/spacedim/Login.kt
+++ app/src/main/java/com/example/spacedim/Login.kt
@@ -1,11 +1,38 @@
 package com.example.spacedim
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.spacedim.databinding.ActivityLoginBinding
 
 class Login : AppCompatActivity() {
+
+
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        //setContentView(R.layout.activity_login)
+        val binding : ActivityLoginBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_login)
+
+        binding.buttonConnection.setOnClickListener {
+            buttonClicked()
+        }
+
+
+    }
+
+
+
+    fun buttonClicked(){
+        //Intent pour ouvrir l'activité suivante
+        val intent = Intent(this, WaitingRoom::class.java)
+        //Lancement de l'intent (changement d'écran)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/spacedim/WaitingRoom.kt
@@ -0,0 +1,11 @@
+package com.example.spacedim
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WaitingRoom : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_waiting_room)
+    }
+}
