b27e92aaa663c00a6f93eb0a3472ac8a1bab4333
Carlitos73490 <71330622+Carlitos73490@users.noreply.github.com>
2021-01-18 16:02:47 +0100

    Nav_Graph WaitingRoom to Dashbord Working

--- app/src/main/java/com/example/spacedim/WaitingRoomFragment.kt
+++ app/src/main/java/com/example/spacedim/WaitingRoomFragment.kt
@@ -1,15 +1,18 @@
 package com.example.spacedim
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import com.example.spacedim.databinding.ActivityWaitingRoomBinding
+import com.example.spacedim.databinding.FragmentLoginBinding
+import com.example.spacedim.databinding.FragmentWaitingRoomBinding
+import okhttp3.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -17,16 +20,15 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class WaitingRoomFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var fragmentWaitingRoomBinding: FragmentWaitingRoomBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+        //setContentView(R.layout.activity_waiting_room)
+
+
     }
 
     override fun onCreateView(
@@ -34,26 +36,45 @@ class WaitingRoomFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_waiting_room, container, false)
+        val binding = FragmentWaitingRoomBinding.inflate(inflater, container, false)
+        fragmentWaitingRoomBinding = binding
+
+        val ws = SocketListener().run()
+
+        binding.buttonStartGame.setOnClickListener {
+            GoDashBoard()
+        }
+        return binding.root
+        //return inflater.inflate(R.layout.fragment_waiting_room, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment WaitingRoomFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            WaitingRoomFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    fun GoDashBoard() {
+        requireActivity().runOnUiThread { // This code will always run on the UI thread, therefore is safe to modify UI elements.
+            NavHostFragment.findNavController(this)
+                .navigate(R.id.action_waitingRoomFragment_to_dashBoardFragment)
+        }
+    }
+
+    class SocketListener : WebSocketListener() {
+        fun run() {
+            println("new web socket")
+            val client = OkHttpClient()
+            val request =
+                Request.Builder()
+                    .url("ws://spacedim.async-agency.com:8081/ws/join/roomTest/1")
+                    .build()
+            val ws = client.newWebSocket(request, this)
+        }
+
+        override fun onOpen(webSocket: WebSocket, response: Response) {
+            println("onOpen")
+            println(response)
+        }
+
+        override fun onMessage(webSocket: WebSocket, response: String) {
+            println("onMessage")
+            println(response)
+        }
     }
 }
+
