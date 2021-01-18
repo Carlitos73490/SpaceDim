bcd18b6db1697f2bdb9cf89de4774827aab6b219
Carlitos73490 <71330622+Carlitos73490@users.noreply.github.com>
2021-01-18 14:58:43 +0100

    Nav_Graph LoginToWaiting Room Working

--- app/src/main/java/com/example/spacedim/LoginFragment.kt
+++ app/src/main/java/com/example/spacedim/LoginFragment.kt
@@ -2,13 +2,12 @@ package com.example.spacedim
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.databinding.DataBindingUtil
-import com.example.spacedim.databinding.ActivityLoginBinding
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
 import com.example.spacedim.databinding.FragmentLoginBinding
 import com.squareup.moshi.JsonClass
 import com.squareup.moshi.Moshi
@@ -26,101 +25,124 @@ import org.json.JSONObject
  * create an instance of this fragment.
  */
 class LoginFragment : Fragment() {
+    private var  fragmentLoginBinding: FragmentLoginBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-            //setContentView(R.layout.activity_login)
-
-            binding.buttonConnection.setOnClickListener {
-                val client = OkHttpClient()
-                val moshi = Moshi.Builder().build()
-                val playerJsonAdapter = moshi.adapter(Player::class.java)
-                val request = Request.Builder()
-                    .url("https://spacedim.async-agency.com/api/user/find/" + binding.editTextLogin.text)
-                    .build()
-                client.newCall(request).enqueue(object : Callback {
-                    override fun onFailure(call: Call, e: IOException) {
-                        e.printStackTrace()
-                    }
-                    override fun onResponse(call: Call, response: Response) {
-                        response.use {
-
-                            if(response.isSuccessful) {
-                                val player = playerJsonAdapter.fromJson(response.body!!.source())
-                                println(player!!.id)
-                                println(player!!.name)
-
-                                GoWaitingRoom()
-                            } else{
-
-
-                                throw IOException("Unexpected code $response")
-                            }
-                        }
-                    }
-                })
-
-            }
-
-            binding.buttonRegister.setOnClickListener {
-
-                // create your json here
-                val jsonObject = JSONObject()
-                try {
-                    jsonObject.put("name", binding.editTextLogin.text)
-                } catch (e: JSONException) {
-                    e.printStackTrace()
-                }
-
-                val client = OkHttpClient()
-                val moshi = Moshi.Builder().build()
-                val playerJsonAdapter = moshi.adapter(Player::class.java)
-                val mediaType = "application/json; charset=utf-8".toMediaType()
-                val body = jsonObject.toString().toRequestBody(mediaType)
-                val request = Request.Builder()
-                    .url("https://spacedim.async-agency.com/api/user/register")
-                    .post(body)
-                    .build()
-                client.newCall(request).enqueue(object : Callback {
-                    override fun onFailure(call: Call, e: IOException) {
-                        e.printStackTrace()
-
-                    }
-                    override fun onResponse(call: Call, response: Response) {
-                        response.use {
-                            if (response.isSuccessful) {
-                                val player = playerJsonAdapter.fromJson(response.body!!.source())
-                                println(player!!.id)
-                                println(player!!.name)
-
-
-                            }else{
-
-                                throw IOException("Unexpected code $response")
-                            }
-                        }
-                    }
-                })
-
-
-            }
-
-
+        //setContentView(R.layout.activity_login)
 
 
     }
-
-    @JsonClass(generateAdapter = true)
-    data class Player(var id: Int?,var name : String?,var avatar: String?,var score: Int?,var state: String?)
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+
+        val binding = FragmentLoginBinding.inflate(inflater, container, false)
+        fragmentLoginBinding = binding
+
+        binding.buttonConnection.setOnClickListener {
+            val client = OkHttpClient()
+            val moshi = Moshi.Builder().build()
+            val playerJsonAdapter = moshi.adapter(Login.Player::class.java)
+            val request = Request.Builder()
+                .url("https://spacedim.async-agency.com/api/user/find/" + binding.editTextLogin.text)
+                .build()
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    e.printStackTrace()
+                }
+                override fun onResponse(call: Call, response: Response) {
+                    response.use {
+
+                        if(response.isSuccessful) {
+                            val player = playerJsonAdapter.fromJson(response.body!!.source())
+                            println(player!!.id)
+                            println(player!!.name)
+
+                            activity!!.runOnUiThread { // This code will always run on the UI thread, therefore is safe to modify UI elements.
+                            Toast.makeText(
+                                activity,
+                                " " + player!!.id + " " + player!!.name + " Connected",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            }
+
+                            GoWaitingRoom()
+                        } else{
+
+
+                            activity!!.runOnUiThread { // This code will always run on the UI thread, therefore is safe to modify UI elements.
+                                binding.textViewResponseHttp.text = "Connection failed $response"
+                            }
+
+                            throw IOException("Unexpected code $response")
+                        }
+                    }
+                }
+            })
+
+        }
+
+        binding.buttonRegister.setOnClickListener {
+
+            // create your json here
+            val jsonObject = JSONObject()
+            try {
+                jsonObject.put("name", binding.editTextLogin.text)
+            } catch (e: JSONException) {
+                e.printStackTrace()
+            }
+
+            val client = OkHttpClient()
+            val moshi = Moshi.Builder().build()
+            val playerJsonAdapter = moshi.adapter(Login.Player::class.java)
+            val mediaType = "application/json; charset=utf-8".toMediaType()
+            val body = jsonObject.toString().toRequestBody(mediaType)
+            val request = Request.Builder()
+                .url("https://spacedim.async-agency.com/api/user/register")
+                .post(body)
+                .build()
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    e.printStackTrace()
+
+                }
+                override fun onResponse(call: Call, response: Response) {
+                    response.use {
+                        if (response.isSuccessful) {
+                            val player = playerJsonAdapter.fromJson(response.body!!.source())
+                            println(player!!.id)
+                            println(player!!.name)
+                            activity!!.runOnUiThread { // This code will always run on the UI thread, therefore is safe to modify UI elements.
+                                binding.textViewResponseHttp.text =
+                                    "Player " + player!!.name + " " + player!!.id + " Sucessfuly created, you can now launch"
+                            }
+
+                        }else{
+
+                            activity!!.runOnUiThread { // This code will always run on the UI thread, therefore is safe to modify UI elements.
+                                binding.textViewResponseHttp.text = "registration failed $response"
+                            }
+                            throw IOException("Unexpected code $response")
+                        }
+                    }
+                }
+            })
+        }
+        return binding.root
+        //return inflater.inflate(R.layout.fragment_login, container, false)
     }
+
+    fun  GoWaitingRoom(){
+        requireActivity().runOnUiThread { // This code will always run on the UI thread, therefore is safe to modify UI elements.
+            NavHostFragment.findNavController(this)
+                .navigate(R.id.action_loginFragment_to_waitingRoomFragment)
+        }
+    }
+
+    @JsonClass(generateAdapter = true)
+    data class Player(var id: Int?,var name : String?,var avatar: String?,var score: Int?,var state: String?)
 
 }
