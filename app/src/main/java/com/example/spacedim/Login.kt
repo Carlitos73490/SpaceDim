a258568d4bda651ec0f213339516044879b9388f
Carlitos73490 <71330622+Carlitos73490@users.noreply.github.com>
2021-01-18 10:35:15 +0100

    Control/Error message added

--- app/src/main/java/com/example/spacedim/Login.kt
+++ app/src/main/java/com/example/spacedim/Login.kt
@@ -3,9 +3,12 @@ package com.example.spacedim
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.spacedim.databinding.ActivityLoginBinding
+import com.squareup.moshi.JsonClass
+import com.squareup.moshi.Moshi
 import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.RequestBody.Companion.toRequestBody
@@ -29,6 +32,8 @@ class Login : AppCompatActivity() {
 
         binding.buttonConnection.setOnClickListener {
             val client = OkHttpClient()
+            val moshi = Moshi.Builder().build()
+            val playerJsonAdapter = moshi.adapter(Player::class.java)
             val request = Request.Builder()
                 .url("https://spacedim.async-agency.com/api/user/find/" + binding.editTextLogin.text)
                 .build()
@@ -38,20 +43,34 @@ class Login : AppCompatActivity() {
                 }
                 override fun onResponse(call: Call, response: Response) {
                     response.use {
-                        if (!response.isSuccessful) throw IOException("Unexpected code $response")
 
-                        for ((name, value) in response.headers) {
-                            println("$name: $value")
+                        if(response.isSuccessful) {
+                            val player = playerJsonAdapter.fromJson(response.body!!.source())
+                            println(player!!.id)
+                            println(player!!.name)
+
+
+                            runOnUiThread {
+                                Toast.makeText(
+                                    applicationContext,
+                                    " " + player!!.id + " " + player!!.name + " Connected",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+
+                            }
+
+                            GoWaitingRoom()
+                        } else{
+
+                            runOnUiThread {
+                                binding.textViewResponseHttp.text = "Connection failed $response"
+                            }
+                            throw IOException("Unexpected code $response")
                         }
-                        println(response.body!!.string())
                     }
                 }
             })
 
-            //Intent pour ouvrir l'activité suivante
-            val intent = Intent(this, WaitingRoom::class.java)
-            //Lancement de l'intent (changement d'écran)
-            startActivity(intent)
         }
 
         binding.buttonRegister.setOnClickListener {
@@ -65,6 +84,8 @@ class Login : AppCompatActivity() {
             }
 
             val client = OkHttpClient()
+            val moshi = Moshi.Builder().build()
+            val playerJsonAdapter = moshi.adapter(Player::class.java)
             val mediaType = "application/json; charset=utf-8".toMediaType()
             val body = jsonObject.toString().toRequestBody(mediaType)
             val request = Request.Builder()
@@ -74,28 +95,46 @@ class Login : AppCompatActivity() {
             client.newCall(request).enqueue(object : Callback {
                 override fun onFailure(call: Call, e: IOException) {
                     e.printStackTrace()
+
                 }
                 override fun onResponse(call: Call, response: Response) {
                     response.use {
-                        if (!response.isSuccessful) throw IOException("Unexpected code $response")
+                        if (response.isSuccessful) {
+                            val player = playerJsonAdapter.fromJson(response.body!!.source())
+                            println(player!!.id)
+                            println(player!!.name)
 
-                        for ((name, value) in response.headers) {
-                            println("$name: $value")
+                            runOnUiThread {
+                                binding.textViewResponseHttp.text =
+                                    "Player " + player!!.name + " " + player!!.id + " Sucessfuly created, you can now launch"
+                            }
+                        }else{
+
+                            runOnUiThread {
+                                binding.textViewResponseHttp.text =
+                                    "Registration failed $response"
+                            }
+                            throw IOException("Unexpected code $response")
                         }
-                        println(response.body!!.string())
                     }
                 }
             })
-            //Intent pour ouvrir l'activité suivante
-            val intent = Intent(this, WaitingRoom::class.java)
-            //Lancement de l'intent (changement d'écran)
-            startActivity(intent)
+
+
         }
+
 
 
     }
 
+    fun  GoWaitingRoom(){
+        //Intent pour ouvrir l'activité suivante
+        val intent = Intent(this, WaitingRoom::class.java)
+        //Lancement de l'intent (changement d'écran)
+        startActivity(intent)
+    }
 
-
+    @JsonClass(generateAdapter = true)
+    data class Player(var id: Int?,var name : String?,var avatar: String?,var score: Int?,var state: String?)
 
 }
