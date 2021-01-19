02183201921d5f6305008b9d41a06b6c451e2e30
Carlitos73490 <71330622+Carlitos73490@users.noreply.github.com>
2021-01-19 09:01:05 +0100

    Navigation Fix (End/Win To finish)

--- app/src/main/java/com/example/spacedim/waitingRoom/WaitingRoomFragment.kt
+++ app/src/main/java/com/example/spacedim/waitingRoom/WaitingRoomFragment.kt
@@ -1,22 +1,15 @@
-package com.example.spacedim
+package com.example.spacedim.waitingRoom
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
+import com.example.spacedim.R
 import com.example.spacedim.databinding.FragmentWaitingRoomBinding
 import okhttp3.*
 
-
-/**
- * A simple [Fragment] subclass.
- * Use the [WaitingRoomFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class WaitingRoomFragment : Fragment() {
 
     private var fragmentWaitingRoomBinding: FragmentWaitingRoomBinding? = null
@@ -49,7 +42,7 @@ class WaitingRoomFragment : Fragment() {
     fun GoDashBoard() {
         requireActivity().runOnUiThread { // This code will always run on the UI thread, therefore is safe to modify UI elements.
             NavHostFragment.findNavController(this)
-                .navigate(R.id.action_loginFragment_to_waitingRoomFragment)
+                .navigate(R.id.action_waitingRoomFragment_to_dashBoardFragment)
         }
     }
 
@@ -75,4 +68,3 @@ class WaitingRoomFragment : Fragment() {
         }
     }
 }
-
